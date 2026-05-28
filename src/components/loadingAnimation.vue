265312881045f5d274af9673c31cf6c4c8594e78
Amirali Shafiee <shafieea197@gmail.com>
2026-05-28 23:34:53 +0330

    change the color of loading animation

--- src/components/loadingAnimation.vue
+++ src/components/loadingAnimation.vue
@@ -1,3 +1,3 @@
 <template>
-    <span class="animate-spin border-2 border-b-blue-600 border-t-transparent rounded-full w-5 h-5"></span>
+    <span class="animate-spin border-2 border-white border-t-transparent rounded-full w-5 h-5"></span>
 </template>
